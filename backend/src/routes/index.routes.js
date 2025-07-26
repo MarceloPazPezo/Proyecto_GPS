e9689ba16914c2c00a2f4aba8ee9cc1aab5acf80
MarceloPazPezo <marcelo.paz2101@alumnos.ubiobio.cl>
2025-07-26 16:00:11 -0400

    fix: Corregir importación del componente PopUpUpdateMural en Biblioteca.jsx y mejorar el formato de index.routes.js

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -5,24 +5,24 @@ import authRoutes from "./auth.routes.js";
 import preguntasRoutes from "./preguntas.routes.js";
 import respuestasRoutes from "./respuestas.routes.js";
 import quizRoutes from "./cuestionario.routes.js";
-import shareRoutes from "./compartido.routes.js"
-import EXRoutes from "./EX.routes.js"
-import muralRoutes from "./mural.routes.js"
+import shareRoutes from "./compartido.routes.js";
+import EXRoutes from "./EX.routes.js";
+import muralRoutes from "./mural.routes.js";
 import notasRoute from "./notes.routes.js";
 import carreraRoutes from "./carrera.routes.js";
 
 const router = Router();
 
 router
-    .use("/auth", authRoutes)
-    .use("/user", userRoutes)
-    .use("/preguntas", preguntasRoutes)
-    .use("/respuestas", respuestasRoutes)
-    .use("/quiz",quizRoutes)
-    .use("/share",shareRoutes)
-    .use("/EX",EXRoutes)
-    .use("/notas",notasRoute)
-    .use("/mural",muralRoutes)
-    .use("/EX",EXRoutes)
-    .use("/carrera", carreraRoutes);
+  .use("/auth", authRoutes)
+  .use("/user", userRoutes)
+  .use("/preguntas", preguntasRoutes)
+  .use("/respuestas", respuestasRoutes)
+  .use("/quiz", quizRoutes)
+  .use("/share", shareRoutes)
+  .use("/notas", notasRoute)
+  .use("/mural", muralRoutes)
+  .use("/EX", EXRoutes)
+  .use("/carrera", carreraRoutes);
+
 export default router;
